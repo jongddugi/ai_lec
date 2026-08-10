7a78c0e2d330c4db792db0d8689fec0edc7c5c86
jongddugi <z9tgjjs6cw@privaterelay.appleid.com>
2026-08-10 21:37:01 +0900

    repractice num2

--- repractice/pytorchEx2.py
+++ repractice/pytorchEx2.py
@@ -20,4 +20,48 @@ print()
 
 t5 = torch.linspace(0, 3, 10)
 print(t5)
-print(0)
+print()
+
+print(torch.exp(t5))
+print()
+print(torch.log(t5))
+print()
+print(torch.cos(t5))
+print()
+print(torch.sin(t5))
+print()
+print(torch.sqrt(t5))
+print()
+print(torch.mean(t5))
+print()
+
+t6 = torch.tensor([[2,3,0], [90, 50, 70]])
+print(t6)
+print()
+print(torch.max(t6))
+print()
+print(torch.max(t6, dim=1))
+#value는 각각 제일 큰 값을 반환, 각 index위치
+#이터레이터 제네레이터 공부 필
+print()
+print(torch.max(t6,dim=1)[1])
+
+print()
+print([3,90])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
